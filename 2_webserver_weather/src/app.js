7553509b9b43bd8222c507cb70ddbcc290c8e66e
raven4ever <wrtv23@gmail.com>
2022-01-04 13:06:49 +0200

    remove port & URL hardcoding

--- 2_webserver_weather/src/app.js
+++ 2_webserver_weather/src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // set static folder
 const publicDir = path.join(__dirname, '../public')
@@ -86,6 +87,6 @@ app.get('*', (req, res) => {
 })
 
 // start express server
-app.listen(3000, () => {
-    console.log('Server is up on port 3000...')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port + '...')
 })
