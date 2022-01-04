cd57419000888e97e8f507c70df3313406f4ca69
raven4ever <wrtv23@gmail.com>
2022-01-04 10:24:57 +0200

    hbs templating

--- 2_webserver_weather/src/app.js
+++ 2_webserver_weather/src/app.js
@@ -3,11 +3,32 @@ const express = require('express')
 
 const app = express()
 
+// set static folder
 const publicDir = path.join(__dirname, '../public')
 app.use(express.static(publicDir))
 
+// set templating engine
+app.set('view engine', 'hbs')
+
+// routes
 app.get('/', (req, res) => {
-    res.send('<h1>Weather!</h1>')
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Adrian'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About HBS',
+        name: 'Adrian'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'Helpful text!'
+    })
 })
 
 app.get('/weather', (req, res) => {
@@ -17,6 +38,7 @@ app.get('/weather', (req, res) => {
     })
 })
 
+// start express server
 app.listen(3000, () => {
     console.log('Server is up on port 3000...')
 })
