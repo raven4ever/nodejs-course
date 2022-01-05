94d3d0299df68d380572a2de02e434c3d7321aa3
raven4ever <wrtv23@gmail.com>
2022-01-05 15:33:30 +0200

    task sorting

--- 3_tasks manager/src/routers/task.js
+++ 3_tasks manager/src/routers/task.js
@@ -7,6 +7,7 @@ router.post('/tasks', auth, async (req, res) => {
     // const tsk = new Task(req.body)
 
     const tsk = new Task({
+        // unpack object
         ...req.body,
         owner: req.user._id
     })
@@ -21,11 +22,18 @@ router.post('/tasks', auth, async (req, res) => {
 
 // GET /tasks?completed=false/true
 // GET /tasks?limit=5&skip=2
+// GET /tasks?sortBy=createdAt:asc/desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
 
     try {
@@ -36,7 +44,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         })
         res.send(req.user.tasks)
