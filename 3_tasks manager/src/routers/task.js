19407d485fe33fb6662cf8fbd7918d931721daa0
raven4ever <wrtv23@gmail.com>
2022-01-05 15:17:11 +0200

    filtering tasks

--- 3_tasks manager/src/routers/task.js
+++ 3_tasks manager/src/routers/task.js
@@ -19,11 +19,21 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+// GET /tasks?completed=false/true
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
     try {
         // const tasks = await Task.find()
         // const tasks = await Task.find({ owner: req.user._id })
-        await req.user.populate('tasks')
+        await req.user.populate({
+            path: 'tasks',
+            match: match
+        })
         res.send(req.user.tasks)
     } catch (error) {
         res.status(500).send()
