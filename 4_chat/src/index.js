6cd0668ff9c349c4abb2c60ef8133d11d4b3fe16
raven4ever <wrtv23@gmail.com>
2022-01-10 11:25:13 +0200

    basic sockets

--- 4_chat/src/index.js
+++ 4_chat/src/index.js
@@ -1,14 +1,23 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 const port = process.env.PORT || 3000
 
 // set static folder
 const publicDir = path.join(__dirname, '../public')
 app.use(express.static(publicDir))
 
+io.on('connection', () => {
+    console.log('New user connected')
+})
+
 // start express server
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server is up on port ' + port + '...')
 })
